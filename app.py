472b6b29532cec9e92e82cea0eda15ad5528da83
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2025-02-16 13:24:44 +0100

    continuing

--- app.py
+++ app.py
@@ -1,50 +1,25 @@
 #!./.env/bin/python3.12
 
-# NOTE:
-# Current idea for implementing a backend for the blog-page:
-#
-# FOR THE FRONT BLOG-PAGE (WHERE ALL THE BLOGS ARE ORGANIZED)
-#  - Use Flask to serve JSON with:
-#       1. Title / header
-#       2. Thumbnail / preview (as src links to the backend)
-#       3. Tags
-#       4. Introduction / TL;DR
-#
-# HOW TO GO ABOUT GETTING JSON DATA FROM THE MARKDOWN FILES THEMSELVES:
-# TO AVOID HAVING TO MANUALLY WRITE THE JSON FILES TO BE SERVED MANUALLY?
-#  - Write some code to fetch the following contents from the MD files.
-#       CON:
-#           - I would have to use some static MD template to make the MD file predictable for the program.
-#       PRO:
-#           - I can dynamically edit the files and the update on the Clinet side would be emediate.
-#
-# - STUFF TO KEEP IN MIND WHEN WRITING THIS:
-#       1. Some system to keep up with tags on posts.
-#
-# PR BLOG POST (THE POSTS THEMSELVES):
-#  - Use Flask to serve static HTML, allready prepared the styling on the client-side with Vue.
-
-from flask import Flask, jsonify, request
-import marko
-
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
-with open("./posts/post-1.md", "r") as f:
-    test_text = f.read()
-
-
-@app.route("/post-1", methods=["GET"])
-def post_1():
+@app.route("/api/BlogViewImage", methods=["GET"])
+def BlogViewImage():
     if request.method == "GET":
-        data = "This is post-1!"
-        return jsonify({"data": data})
+        return """
+            <p>showing some text, then image</p>
+            <img src='http://localhost:5000/static/image.jpeg' width='500'>
+            """
 
-
-@app.route("/post-2", methods=["GET"])
-def post_2():
+@app.route("/api/AllBlogPosts", methods=["GET"])
+def AllBlogPosts():
     if request.method == "GET":
-        data = "This is post-2!"
-        return jsonify({"data": data})
+        return """
+            <p>Here, we are going to compose all<br>blog posts found on the server!</p>
+            """
 
+@app.route("/api/AllBlogPostsData", methods=["GET"])
+def BlogPostsData():
+    if request.method == "GET":
+        return jsonify({"some": "data", "goes": "here"})
